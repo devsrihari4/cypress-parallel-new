c18ddaf6679b58ef291b16a45a0a65d64d3bebbb
sri <sri@sris-MBP.attlocal.net>
2025-04-23 22:03:52 -0500

    update config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,11 @@
-const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+// import allureWriter from "@shelex/cypress-allure-plugin/writer";
 
 module.exports = defineConfig({
-  e2e: {
-    reporter: 'mochawesome',
-    reporterOptions: {
-      reportDir: 'cypress/reports',
-      overwrite: false,
-      html: false,
-      json: true,
-    },
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      require('@shelex/cypress-allure-plugin/writer')(on, config);
-      return config;
-    },
-  },
+    e2e: {
+        setupNodeEvents(on, config) {
+            allureWriter(on, config);
+            return config;
+        }
+    }
 });

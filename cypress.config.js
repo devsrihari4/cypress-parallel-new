de41383d7c6719d389b82528e3d610c9f70716f3
sri <sri@sris-MBP.attlocal.net>
2025-05-08 14:06:31 -0500

    Update allure report

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,14 @@
 const { defineConfig } = require('cypress');
+const { allureCypress } = require('allure-cypress/reporter');
 
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+// const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 // import allureWriter from "@shelex/cypress-allure-plugin/writer";
 
 module.exports = defineConfig({
     e2e: {
         setupNodeEvents(on, config) {
-            allureWriter(on, config);
+            // allureWriter(on, config);
+            allureCypress(on, config);
             return config;
         }
     }
